57b915953185abe29524738b9950da2943a887c8
Marco Martins <marcomartins86@gmail.com>
2024-04-29 17:59:10 +0100

    Added unit tests for money
    Signed-off-by: Marco Martins <marcomartins86@gmail.com>

--- CDRSandbox/Services/Models/ValueObjects/Money.cs
+++ CDRSandbox/Services/Models/ValueObjects/Money.cs
@@ -7,7 +7,7 @@ public class Money(double value, string currency) : ValueObject
     private const int DefaultDecimalPlaces = 3;
 
     public float Amount => (float)Math.Round(value, DefaultDecimalPlaces, MidpointRounding.ToEven);
-    public Currency Currency { get; } = new(currency); // TODO: need to make constructor to exchange to GBP
+    public Currency Currency { get; } = new(currency);
 
     protected override IEnumerable<object> GetEqualityComponents()
     {
---
+++ CDRSandboxTests/UnitTests/Services/ValueObjects/MoneyFixture.cs
@@ -0,0 +1,107 @@
+﻿using CDRSandbox.Services.Models.ValueObjects;
+using CDRSandboxTests.Base;
+using NUnit.Framework;
+
+namespace CDRSandboxTests.UnitTests.Services.ValueObjects;
+
+[TestFixture]
+public class MoneyFixture : RandomDataGeneratorsBase
+{
+    [Test]
+    public void Creation_MoneyValid()
+    {
+        // Act
+        // Assert
+        for (int i = 0; i < 10000; i++) // let's test 10000 random values just to make sure
+        {
+            var currency = RandomCurrency;
+            var cost = RandomCost;
+            Assert.DoesNotThrow(() => new Money(cost, currency),
+                "Creation with valid pattern must not throw. Error money [{0}][{1}]", cost, currency);
+        }
+    }
+    
+    [Test]
+    public void Creation_Money_CurrencyNull()
+    {
+        // Act
+        // Assert
+        Assert.Throws<Exception>(() => new Money(RandomCost, null!), "Must throw with null currency");
+    }
+    
+    [Test]
+    public void Creation_Money_CurrencyInvalid()
+    {
+        // Act
+        // Assert
+        Assert.Throws<Exception>(() => new Money(RandomCost, "ZZZ"), "Must throw with invalid currency");
+    }
+    
+    [Test]
+    public void Equality()
+    {
+        // Arrange
+        var currency = RandomCurrency;
+        var cost = RandomCost;
+        var one = new Money(cost, currency);
+        var two = new Money(cost, currency);
+        
+        // Act
+        // Assert
+        Assert.That(one == two, Is.True, "Must be equal");
+        Assert.That(one.Equals(two), Is.True,"Must be equal");
+        Assert.That(Equals(one, two), Is.True,"Must be equal");
+        Assert.That(EqualityComparer<Money>.Default.Equals(two, two), Is.True,"Must be equal");
+        Assert.That(one, Is.EqualTo(two), "Must be equal");
+    }
+    
+    [Test]
+    public void Inequality()
+    {
+        // Arrange
+        var one = new Money(RandomCost, RandomCurrency);
+        var two = new Money(RandomCost, RandomCurrency);
+        
+        // Act
+        // Assert
+        Assert.That(one != two, Is.True, "Must be different");
+    }
+    
+    [Test]
+    public void Inequality_SameCost()
+    {
+        // Arrange
+        var cost = RandomCost;
+        var one = new Money(cost, RandomCurrency);
+        var two = new Money(cost, RandomCurrency);
+        
+        // Act
+        // Assert
+        Assert.That(one != two, Is.True, "Must be different");
+    }
+    
+    [Test]
+    public void Inequality_SameCurrency()
+    {
+        // Arrange
+        var currency = RandomCurrency;
+        var one = new Money(RandomCost, currency);
+        var two = new Money(RandomCost, currency);
+        
+        // Act
+        // Assert
+        Assert.That(one != two, Is.True, "Must be different");
+    }
+    
+    [Test]
+    public void ToStringMethod()
+    {
+        // Arrange
+        var currency = RandomCurrency;
+        var cost = Math.Round(RandomCost, 3, MidpointRounding.ToEven);
+        
+        // Act
+        // Assert
+        Assert.That(new Money(cost, currency).ToString(), Is.EqualTo($"{cost} {currency}"), "Must be equal");
+    }
+}
